238ce3452138b8f570726fa4247ab48ccfdfb52a
morgane-rossi <morgane_rt@hotmail.com>
2025-01-15 14:08:03 +0100

    feat : calculator with basic operations and basic error handling

---
+++ calculator.py
@@ -0,0 +1,58 @@
+"""
+Features 1 arithmetic operation : + ; - ; * ; /
+Between 2 numbers
+The numbers are integers or floating numbers
+"""
+
+def enter_value() :
+    number = input("veuillez rentrer un nombre :\n")
+    try :
+        number = float(number)
+        return number
+    except ValueError:
+        print("erreur de saisie")
+
+def enter_symbol():
+    symbol = -1
+    while symbol not in ("+", "-", "*", "/"):
+        print("veuillez choisir une opération : + ; - ; * : /")
+        symbol = input()
+        if symbol not in ("+", "-", "*", "/"):
+            print("opération non conforme, veuillez recommencer")
+    return symbol
+
+def calculate():
+    result = -1
+    try :
+        first = enter_value()
+        symbol = enter_symbol()
+        second = enter_value()
+        if first == None or symbol == None or second == None :
+            raise ValueError("erreur de saisie")
+
+        if symbol == "+":
+            result = first + second
+        elif symbol == "-":
+            result = first - second
+        elif symbol == "*":
+            result = first * second
+        elif symbol == "/":
+            result = first / second
+        print(f"résultat de {first} {symbol} {second} = {result}")
+
+    except ArithmeticError :
+        print("Impossible de diviser par zéro")
+
+    except ValueError:
+        print("erreur saisie")
+
+
+def menu():
+    pass
+
+
+def main():
+    while True :
+        calculate()
+
+main()
