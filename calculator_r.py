c993107f606230e383f069b2e9dc6275f398c57b
morgane-rossi <morgane_rt@hotmail.com>
2025-01-17 10:36:53 +0100

    program can write result in history file, read and reset history file

--- calculator_r.py
+++ calculator_r.py
@@ -4,9 +4,17 @@ from pathlib import Path
 Features 1 arithmetic operation : + ; - ; * ; /
 Between 2 numbers
 The numbers are integers or floating numbers
+Computes and displays the results
+
+History text file to record operations
+Program can read, write in and reset history
 """
 
 def enter_value() :
+    """
+    Entering a numeric value : integer or floating number
+    Not enabling entering another type value
+    """
     number = input("Please enter a number :\n")
     try :
         number = number.replace(",", ".")
@@ -16,6 +24,9 @@ def enter_value() :
         print("Input error")
 
 def enter_symbol():
+    """
+    Entering a symbol for computing : '+', '-', '*', '/'
+    """
     symbol = -1
     while symbol not in ("+", "-", "*", "/"):
         print("Please enter a symbol : + ; - ; * : /")
@@ -25,6 +36,7 @@ def enter_symbol():
     return symbol
 
 def calculate():
+
     result = -1
     try :
         first = enter_value()
@@ -45,15 +57,19 @@ def calculate():
             result = int(result)
         else :
             result = f"{result:.3f}"
+        
+        # displays result with a formatted string
         line = f"Result : {first} {symbol} {second} = {result}\n"
         print(line)
+        # formatted result is stored in history file history.txt
         write_history(line)
 
+    # Errors & exceptions handling
     except ZeroDivisionError :
         print("Dividing by zero : forbidden")
 
     except ArithmeticError :
-        print("Impossible calculation")
+        print("Calculating is impossible")
 
     except ValueError:
         print("Input error")
@@ -61,35 +77,59 @@ def calculate():
     except AttributeError:
         print("Error")
 
-
 def write_history(line):
-    history_f = Path("./history.txt")
 
-    # si le fichier history.txt existe déjà
-    if history_f.is_file():
-        with open("history.txt", "a") as history_f:
-            history_f.write(line)
+    try :
+        history_f = Path("./history.txt")
 
-    else :
-        # si le fichier history n'existe pas encore
-        print("il faut créér le fichier")
-        with open("history.txt", "w") as history_f :
-            history_f.write(line)
+        # if history.txt text already exists in same directory
+        if history_f.is_file():
+            with open("history.txt", "a") as history_f:
+                history_f.write(line)
 
+        else :
+            # if history.txt text doesn't already exists
+            with open("history.txt", "w") as history_f :
+                history_f.write(line)
+
+    except (FileNotFoundError, NameError, PermissionError, IsADirectoryError, IOError, UnicodeDecodeError, UnicodeEncodeError, OSError, OverflowError) as error :
+        print("Error while writing")
+
+    except Exception :
+        print("Error while writing")
 
 def read_history():
-    # voir toutes les opérations arithmétiques effectuées par l'utilisateur
-    # possibilité d’effacer  cet  historique
-    # possibilité de réinitialiser cet  historique
-    print("\nHistorique de la calculatrice :\n")
 
-    history_f = Path("./history.txt")
-    if history_f.is_file():
-        with open("history.txt", "r", encoding="utf-8") as history_f :
-            print(history_f.read())
-        print()
-    else :
-        print("historique vide")
+    try :
+        print("\nCalculator history :\n")
+
+        history_f = Path("./history.txt")
+        if history_f.is_file():
+            with open("history.txt", "r") as history_f :
+                print(history_f.read())
+            print()
+        else :
+            print("Empty history\n")
+
+    except (FileNotFoundError, NameError, PermissionError, IsADirectoryError, IOError, UnicodeDecodeError, UnicodeEncodeError, OSError, OverflowError) as error :
+        print("Error while reading")
+
+    except Exception :
+        print("Error while reading")
+
+def reset_history():
+
+    try:
+        history_f = Path("./history.txt")
+        with open("history.txt", "w") as history_f :
+            history_f.write("")
+        print("History reset\n")
+
+    except (FileNotFoundError, NameError, PermissionError, IsADirectoryError, IOError, UnicodeDecodeError, UnicodeEncodeError, OSError, OverflowError) as error :
+        print("Error while writing")
+
+    except Exception :
+        print("Error while writing")
 
 def program_run():
     try :
@@ -98,6 +138,9 @@ def program_run():
             read_history()
             program_run()
         elif fonctions == "3":
+            reset_history()
+            program_run()
+        elif fonctions == "4":
             return
         elif fonctions == "1":
             while True:
@@ -110,13 +153,15 @@ def menu():
     print("Calculator menu\nWhat is your choice ?")
     print("Choice 1 : using the calculator")
     print("Choice 2 : displaying history")
-    print("Choice 3 : quit")
+    print("Choice 3 : resetting history")
+    print("Choice 4 : quit")
     menu = input()
-    while menu not in ("1", "2", "3"):
+    while menu not in ("1", "2", "3", "4"):
         print("Sorry I didn't understand")
         print("Choice 1 : using the calculator")
         print("Choice 2 : displaying history")
-        print("Choice 3 : quit")
+        print("Choice 3 : resetting history")
+        print("Choice 4 : quit")
         menu = input()
     return menu
 
